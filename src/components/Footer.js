a559a9c3ab5395286ecaffe399caa90eaaddc5da
priom7 <priom7197@gmail.com>
2025-02-20 21:57:06 +0000

    Chat-W-Data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="footer" id="contact">
-      <p>&copy; {new Date().getFullYear()} Chat‑W‑Data. All rights reserved.</p>
+      <p>&copy; {new Date().getFullYear()} Chat‑W‑Data. Md Sharif Alam. All rights reserved.</p>
     </footer>
   );
 }
